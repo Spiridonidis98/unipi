26165023bda6a0af5e874ff8fc00fd53bcec494b
koutsioj <64408332+koutsioj@users.noreply.github.com>
2024-02-01 05:42:59 +0200

    added forgotten route to app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const database = require('./database.js');
 const express = require('express');
 const userRoute = require('./routes/user.js')
+const movieRoute = require('./routes/movie.js')
 database.database();
 const app = express ();
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use((req, res, next) =>{
 });
 
 app.use('/api/v1/user',userRoute);
+app.use('/api/v1/movies',movieRoute);
+
 app.listen(8080 , () => {
     console.log('Server listens to port 8080')
 });
--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const movieController = require(`${__dirname}/../controllers/movieController`);
 
-router.route('/movies')
+router.route('/')
     .get(movieController.getAllMovies)
     .post(movieController.createMovie)
 
-router.route('/movies/:lang/:id')
+router.route(':lang/:id')
     .get(movieController.getMovieById)
     .patch(movieController.updateMovieById)
     .delete(movieController.deleteMovieById)
 
-router.route('/movies/:lang/:type')
+router.route(':lang/:type')
     .get(movieController.getMoviesByType)
 
 module.exports = router;
